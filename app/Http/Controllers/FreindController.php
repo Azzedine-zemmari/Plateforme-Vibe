250f60b41055ce64dcf2c4b7f8e84c2734175a43
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-02-24 18:56:45 +0000

    add the addFreing functionnality

--- app/Http/Controllers/FreindController.php
+++ app/Http/Controllers/FreindController.php
@@ -7,6 +7,7 @@ use App\Events\FriendRequestSent;
 
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Exists;
 
 class FreindController extends Controller
 {
@@ -15,7 +16,7 @@ class FreindController extends Controller
         $authenticatedId = Auth::id();
         $freinds = Freind::join('users','users.id','=','freinds.friend_id')
         ->where('freinds.user_id','=',$authenticatedId)
-        ->select('users.id','users.name','users.email','users.pseudo','users.image')
+        ->select('users.id','users.name','users.email','users.pseudo','users.image','freinds.status')
         ->get();
 
         return view('users.Myfreinds',compact('freinds'));
@@ -23,17 +24,39 @@ class FreindController extends Controller
     public function addFreind(Request $request){
         $freindId = $request->idUser;
         $userId = Auth::id();
-        $existingFreind = Freind::where('user_id',$userId)->where('friend_id',$freindId)->first();
+        $existingFreind = Freind::where('user_id',$userId)
+        ->where('friend_id',$freindId)
+        ->orWhere('user_id',$freindId)
+        ->where('friend_id',$userId)
+        ->exists();
         if($existingFreind){
-            return redirect()->route('Myfreinds')->with('error','This user is already exists');
+            if($existingFreind->status = 'done'){
+                return redirect()->route('Myfreinds')->with('error','You are already freinds');
+            }
+            if($existingFreind->status = 'pending'){
+                return redirect()->route('Myfreinds')->with('error','A freind request is already pending');
+            }
+            if($existingFreind->status = 'canceled'){
+                return redirect()->route('Myfreinds')->with('error','This friend request was previously rejected.');
+            }
         }
+
 
         Freind::create([
             'user_id' => $userId,
-            'friend_id' => $freindId
+            'friend_id' => $freindId,
+            'status' => 'pending'
+        ]);
+        Freind::create([
+            'user_id' => $freindId,
+            'friend_id' => $userId,
+            'status' => 'pending'
         ]);
 
         
         return redirect()->route('Myfreinds')->with('success','freind added successfuly');
+    }
+    public function acceptFreind(Request $request){
+        
     }
 }
