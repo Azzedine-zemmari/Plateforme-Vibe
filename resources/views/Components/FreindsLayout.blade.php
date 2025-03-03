be2d671d6df92a7923e22424ac52676085c1df23
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-03-03 12:21:34 +0000

    drop gitignore

--- resources/views/Components/FreindsLayout.blade.php
+++ resources/views/Components/FreindsLayout.blade.php
@@ -14,7 +14,7 @@
             <div class="flex justify-between h-16">
                 <div class="flex">
                     <div class="flex-shrink-0 flex items-center">
-                        <h1 class="text-xl font-bold text-indigo-600"><a href="{{route('posts')}}">UserHub</a></h1>
+                        <h1 class="text-xl font-bold text-indigo-600"><a href="{{route('posts')}}">Vibe</a></h1>
                     </div>
                     <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
                         <a href="{{url('/dashboard')}}" class="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
